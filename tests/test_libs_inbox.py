2b124d2b7a903a7952de8585e113ee14837d4da4
Walter Weinmann <walter.weinmann@gmail.com>
2022-02-10 18:48:10 +0100

    Processing the input directories.

--- tests/test_libs_inbox.py
+++ tests/test_libs_inbox.py
@@ -64,11 +64,11 @@ def test_file_extension_pdf_ok_protected(fxtr_new_db_empty_inbox):
 
     dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
 
-    # Directory inbox ist empty
-    assert not os.listdir(inbox)
-    # Directory inbox_accepted contains exactly one file
-    assert len(os.listdir(inbox_accepted)) == 1
-    assert os.path.isfile(inbox_accepted + "/" + file_target)
+    # Directory inbox contains exactly one file
+    assert len(os.listdir(inbox)) == 1
+    assert os.path.isfile(inbox_accepted + "/" + file_source)
+    # Directory inbox_accepted ist empty
+    assert not os.listdir(inbox_accepted)
     # Directory inbox_rejected ist empty
     assert not os.listdir(inbox_rejected)
 
@@ -96,3 +96,31 @@ def test_file_unknown_file_extension_ok(fxtr_new_db_empty_inbox):
     # Directory inbox_rejected contains exactly one file
     assert len(os.listdir(inbox_rejected)) == 1
     assert os.path.isfile(inbox_rejected + "/" + file_target)
+
+
+# -----------------------------------------------------------------------------
+# Test File Extension: xxx - unknown_file_extension_protected.
+# -----------------------------------------------------------------------------
+@pytest.mark.issue
+def test_file_unknown_file_extension_ok_protected(fxtr_new_db_empty_inbox):
+    """Test: pdf_text_ok."""
+    file_source = "unknown_file_extension_protected.xxx"
+    file_target = "unknown_file_extension_protected_1.xxx"
+
+    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX] + "/"
+    inbox_accepted = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_ACCEPTED]
+    inbox_rejected = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_REJECTED]
+
+    shutil.copy(TESTS_INBOX + file_source, inbox)
+
+    os.chmod(inbox + file_source, stat.SF_IMMUTABLE)
+
+    dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
+
+    # Directory inbox contains exactly one file
+    assert len(os.listdir(inbox)) == 1
+    assert os.path.isfile(inbox_accepted + "/" + file_source)
+    # Directory inbox_accepted ist empty
+    assert not os.listdir(inbox_accepted)
+    # Directory inbox_rejected ist empty
+    assert not os.listdir(inbox_rejected)
