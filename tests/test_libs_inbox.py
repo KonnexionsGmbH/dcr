8655dc1e4df06107bc7d44bdf3d8e063c9611a4a
Walter Weinmann <walter.weinmann@gmail.com>
2022-02-11 08:45:44 +0100

    Processing the input directories.

--- tests/test_libs_inbox.py
+++ tests/test_libs_inbox.py
@@ -21,34 +21,50 @@ dcr.initialise_logger()
 
 
 # -----------------------------------------------------------------------------
+# Show the state of the inboxes after the test.
+# -----------------------------------------------------------------------------
+def show_inboxes_after(
+    inbox: str, inbox_accepted: str, inbox_rejected: str
+) -> None:
+    """Show the state of the inboxes after the test.
+
+    Args:
+        inbox (str): File directory inbox.
+        inbox_accepted (str): File directory inbox_accepted.
+        inbox_rejected (str): File directory inbox_rejected.
+    """
+    # Show the content of the inbox directories.
+    print("after: inbox=         ", os.listdir(inbox))
+    print("after: inbox_accepted=", os.listdir(inbox_accepted))
+    print("after: inbox_rejected=", os.listdir(inbox_rejected))
+
+
+# -----------------------------------------------------------------------------
 # Test File Extension: pdf - pdf_text_ok.
 # -----------------------------------------------------------------------------
+@pytest.mark.issue
 def test_file_extension_pdf_ok(fxtr_new_db_empty_inbox):
-    """Test: pdf_text_ok."""
-    file_source = "pdf_text_ok.pdf"
-    file_target = "pdf_text_ok_1.pdf"
+    """Test: pdf - text.
 
+    Due original file is expected in the file directory inbox_accepted.
+    """
     inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX]
     inbox_accepted = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_ACCEPTED]
     inbox_rejected = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_REJECTED]
 
-    shutil.copy(os.path.join(TESTS_INBOX, file_source), inbox)
+    file_inbox = os.path.join(inbox, "pdf_text_ok.pdf")
+    file_source = os.path.join(TESTS_INBOX, "pdf_text_ok.pdf")
+    file_target = os.path.join(inbox_accepted, "pdf_text_ok_1.pdf")
 
-    print("before: inbox=         ",os.listdir(inbox))
+    shutil.copy(file_source, inbox)
 
-    assert len(os.listdir(inbox)) == 1, "before: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "before: inbox should contain source file"
+    verify_inboxes_before(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
     dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
 
-    print("after: inbox=         ",os.listdir(inbox))
-    print("after: inbox_accepted=",os.listdir(inbox_accepted))
-    print("after: inbox_rejected=",os.listdir(inbox_rejected))
-
-    assert not os.listdir(inbox), "after: inbox should be empty"
-    assert len(os.listdir(inbox_accepted)) == 1, "after: inbox_accepted should contain one file"
-    assert os.path.isfile(os.path.join(inbox_accepted , file_target)), "after: inbox should contain target file"
-    assert not os.listdir(inbox_rejected), "after: inbox_rejected should be empty"
+    verify_inbox_accepted_after(
+        inbox, inbox_accepted, inbox_rejected, file_target
+    )
 
 
 # -----------------------------------------------------------------------------
@@ -56,96 +72,209 @@ def test_file_extension_pdf_ok(fxtr_new_db_empty_inbox):
 # -----------------------------------------------------------------------------
 @pytest.mark.issue
 def test_file_extension_pdf_ok_protected(fxtr_new_db_empty_inbox):
-    """Test: pdf_text_ok."""
-    file_source = "pdf_text_ok_protected.pdf"
-    file_target = "pdf_text_ok_protected_1.pdf"
+    """Test: pdf - text - protected.
 
-    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX] + "/"
+    Due to the write protection, the original file should remain
+    unchanged in the inbox.
+    """
+    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX]
     inbox_accepted = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_ACCEPTED]
     inbox_rejected = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_REJECTED]
 
-    shutil.copy(os.path.join(TESTS_INBOX, file_source), inbox)
+    file_inbox = os.path.join(inbox, "pdf_text_ok_protected.pdf")
+    file_source = os.path.join(TESTS_INBOX, "pdf_text_ok_protected.pdf")
 
-    os.chmod(os.path.join(TESTS_INBOX, file_source), stat.SF_IMMUTABLE)
-    
-    print("before: inbox=         ",os.listdir(inbox))
+    shutil.copy(file_source, inbox)
 
-    assert len(os.listdir(inbox)) == 1, "before: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "before: inbox should contain source file"
+    os.chmod(file_inbox, stat.SF_IMMUTABLE)
+
+    verify_inboxes_before(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
     dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
 
-    print("after: inbox=         ",os.listdir(inbox))
-    print("after: inbox_accepted=",os.listdir(inbox_accepted))
-    print("after: inbox_rejected=",os.listdir(inbox_rejected))
-
-    assert len(os.listdir(inbox)) == 1, "after: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "after: inbox should contain source file"
-    assert not os.listdir(inbox_accepted), "after: inbox_accepted should be empty"
-    assert not os.listdir(inbox_rejected), "after: inbox_rejected should be empty"
+    verify_inbox_after(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
 
 # -----------------------------------------------------------------------------
 # Test File Extension: xxx - unknown_file_extension.
 # -----------------------------------------------------------------------------
-def test_file_unknown_file_extension_ok(fxtr_new_db_empty_inbox):
-    """Test: pdf_text_ok."""
-    file_source = "unknown_file_extension.xxx"
-    file_target = "unknown_file_extension_1.xxx"
+@pytest.mark.issue
+def test_file_extension_unknown_ok(fxtr_new_db_empty_inbox):
+    """Test: xxx.
 
-    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX] + "/"
+    Due to the unknown file extension, the original file is expected
+    in the file directory inbox_rejected.
+    """
+    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX]
     inbox_accepted = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_ACCEPTED]
     inbox_rejected = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_REJECTED]
 
-    shutil.copy(os.path.join(TESTS_INBOX, file_source), inbox)
+    file_inbox = os.path.join(inbox, "unknown_file_extension.xxx")
+    file_source = os.path.join(TESTS_INBOX, "unknown_file_extension.xxx")
+    file_target = os.path.join(inbox_accepted, "unknown_file_extension_1.xxx")
 
-    print("before: inbox=         ",os.listdir(inbox))
+    shutil.copy(file_source, inbox)
 
-    assert len(os.listdir(inbox)) == 1, "before: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "before: inbox should contain source file"
+    verify_inboxes_before(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
     dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
 
-    print("after: inbox=         ",os.listdir(inbox))
-    print("after: inbox_accepted=",os.listdir(inbox_accepted))
-    print("after: inbox_rejected=",os.listdir(inbox_rejected))
-
-    assert not os.listdir(inbox), "after: inbox should be empty"
-    assert len(os.listdir(inbox_accepted)) == 1, "after: inbox_accepted should contain one file"
-    assert os.path.isfile(os.path.join(inbox_accepted , file_target)), "after: inbox should contain target file"
-    assert not os.listdir(inbox_rejected), "after: inbox_rejected should be empty"
+    verify_inbox_rejected_after(
+        inbox, inbox_accepted, inbox_rejected, file_target
+    )
 
 
 # -----------------------------------------------------------------------------
 # Test File Extension: xxx - unknown_file_extension_protected.
 # -----------------------------------------------------------------------------
 @pytest.mark.issue
-def test_file_unknown_file_extension_ok_protected(fxtr_new_db_empty_inbox):
-    """Test: pdf_text_ok."""
-    file_source = "unknown_file_extension_protected.xxx"
-    file_target = "unknown_file_extension_protected_1.xxx"
+def test_file_extension_unknown_ok_protected(fxtr_new_db_empty_inbox):
+    """Test: xxx - protected.
 
-    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX] + "/"
+    Due to the write protection, the original file should remain
+    unchanged in the inbox.
+    """
+    inbox = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX]
     inbox_accepted = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_ACCEPTED]
     inbox_rejected = libs.cfg.config[libs.cfg.DCR_CFG_DIRECTORY_INBOX_REJECTED]
 
-    shutil.copy(os.path.join(TESTS_INBOX, file_source), inbox)
+    file_inbox = os.path.join(inbox, "unknown_file_extension_protected.pdf")
+    file_source = os.path.join(
+        TESTS_INBOX, "unknown_file_extension_protected.pdf"
+    )
 
-    os.chmod(os.path.join(TESTS_INBOX, file_source), stat.SF_IMMUTABLE)
+    shutil.copy(file_source, inbox)
 
-    print("before: inbox=         ",os.listdir(inbox))
+    os.chmod(file_inbox, stat.SF_IMMUTABLE)
 
-    assert len(os.listdir(inbox)) == 1, "before: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "before: inbox should contain source file"
+    verify_inboxes_before(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
     dcr.main([libs.cfg.DCR_ARGV_0, libs.cfg.RUN_ACTION_PROCESS_INBOX])
 
-    print("after: inbox=         ",os.listdir(inbox))
-    print("after: inbox_accepted=",os.listdir(inbox_accepted))
-    print("after: inbox_rejected=",os.listdir(inbox_rejected))
+    verify_inbox_after(inbox, inbox_accepted, inbox_rejected, file_inbox)
 
+
+# -----------------------------------------------------------------------------
+# Show and verify the state of the inbox after the test.
+# -----------------------------------------------------------------------------
+def verify_inbox_after(
+    inbox: str, inbox_accepted: str, inbox_rejected: str, file_inbox: str
+) -> None:
+    """Show and verify the state of the inbox after the test.
+
+    Args:
+        inbox (str): File directory inbox.
+        inbox_accepted (str): File directory inbox_accepted.
+        inbox_rejected (str): File directory inbox_rejected.
+        file_inbox (str): The test file in the file directory input.
+    """
+    # Show the content of the inbox directories.
+    show_inboxes_after(inbox, inbox_accepted, inbox_rejected)
+
+    # Verify the content of the inbox directories.
     assert len(os.listdir(inbox)) == 1, "after: inbox should contain one file"
-    assert os.path.isfile(os.path.join(inbox , file_source)), "after: inbox should contain source file"
-    assert not os.listdir(inbox_accepted), "after: inbox_accepted should be empty"
-    assert not os.listdir(inbox_rejected), "after: inbox_rejected should be empty"
+    assert os.path.isfile(
+        file_inbox
+    ), "after: inbox should contain source file"
+    assert not os.listdir(
+        inbox_accepted
+    ), "after: inbox_accepted should be empty"
+    assert not os.listdir(
+        inbox_rejected
+    ), "after: inbox_rejected should be empty"
 
+
+# -----------------------------------------------------------------------------
+# Show and verify the state of the inbox_accepted after the test.
+# -----------------------------------------------------------------------------
+def verify_inbox_accepted_after(
+    inbox: str, inbox_accepted: str, inbox_rejected: str, file_target: str
+) -> None:
+    """Show and verify the state of the inbox_accepted after the test.
+
+    Args:
+        inbox (str): File directory inbox.
+        inbox_accepted (str): File directory inbox_accepted.
+        inbox_rejected (str): File directory inbox_rejected.
+        file_target (str): Expected file in the file directory input_accepted.
+    """
+    # Show the content of the inbox directories.
+    show_inboxes_after(inbox, inbox_accepted, inbox_rejected)
+
+    # Verify the content of the inbox directories.
+    assert not os.listdir(inbox), "after: inbox should be empty"
+    assert (
+        len(os.listdir(inbox_accepted)) == 1
+    ), "after: inbox_accepted should contain one file"
+    assert os.path.isfile(
+        file_target
+    ), "after: inbox_accepted should contain target file"
+    assert not os.listdir(
+        inbox_rejected
+    ), "after: inbox_rejected should be empty"
+
+
+# -----------------------------------------------------------------------------
+# Show and verify the state of the inbox_rejected after the test.
+# -----------------------------------------------------------------------------
+def verify_inbox_rejected_after(
+    inbox: str, inbox_accepted: str, inbox_rejected: str, file_target: str
+) -> None:
+    """Show and verify the state of the inbox_rejected after the test.
+
+    Args:
+        inbox (str): File directory inbox.
+        inbox_accepted (str): File directory inbox_accepted.
+        inbox_rejected (str): File directory inbox_rejected.
+        file_target (str): Expected file in the file directory input_rejected.
+    """
+    # Show the content of the inbox directories.
+    show_inboxes_after(inbox, inbox_accepted, inbox_rejected)
+
+    # Verify the content of the inbox directories.
+    assert not os.listdir(inbox), "after: inbox should be empty"
+    assert not os.listdir(
+        inbox_accepted
+    ), "after: inbox_accepted should be empty"
+    assert (
+        len(os.listdir(inbox_rejected)) == 1
+    ), "after: inbox_rejected should contain one file"
+    assert os.path.isfile(
+        file_target
+    ), "after: inbox_rejected should contain target file"
+
+
+# -----------------------------------------------------------------------------
+# Show and verify the state of the inboxes before the test.
+# -----------------------------------------------------------------------------
+def verify_inboxes_before(
+    inbox: str, inbox_accepted: str, inbox_rejected: str, file_inbox: str
+) -> None:
+    """Show and verify the state of the inboxes before the test.
+
+    Args:
+        inbox (str): File directory inbox.
+        inbox_accepted (str): File directory inbox_accepted.
+        inbox_rejected (str): File directory inbox_rejected.
+        file_inbox (str): The test file in the file directory input.
+    """
+    # Show the content of the inbox directories.
+    print("before: inbox=         ", os.listdir(inbox))
+    if os.path.isdir(inbox_accepted):
+        print("before: inbox_accepted=", os.listdir(inbox_accepted))
+    if os.path.isdir(inbox_rejected):
+        print("before: inbox_rejected=", os.listdir(inbox_rejected))
+
+    # Verify the content of the inbox directories.
+    assert len(os.listdir(inbox)) == 1, "before: inbox should contain one file"
+    assert os.path.isfile(
+        file_inbox
+    ), "before: inbox should contain source file"
+    if os.path.isdir(inbox_accepted):
+        assert not os.listdir(
+            inbox_accepted
+        ), "before: inbox_accepted should be empty"
+    if os.path.isdir(inbox_rejected):
+        assert not os.listdir(
+            inbox_rejected
+        ), "before: inbox_rejected should be empty"
